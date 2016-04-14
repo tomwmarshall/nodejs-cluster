d373f439f7fcc51d8affeb14b6e3cbfd19ab5c70
Tom Marshall <zbva946@live.rhul.ac.uk>
2016-04-14 15:43:16 +0100

    Few alterations/additions. Added functionality but currently commented out of use. Starting to add listeners in.

--- js/simple.js
+++ js/simple.js
@@ -1,11 +1,29 @@
 var cluster = require('cluster');
 var http = require('http');
-var numCPUs = 4;
+var numCPUs = require('os').cpus().length;
+
+// go to http://127.0.0.1:8000/
 
 if (cluster.isMaster) {
     for (var i = 0; i < numCPUs; i++) {
-        cluster.fork();
+        var workers = [];
+        workers.push(cluster.fork());
+        console.log('New worker created from master.');
     }
+
+    cluster.on('death', function(worker) {
+        console.log('worker ' + worker.pid + ' died');
+    });
+/*
+    process.on('SIGINT', function(){
+        console.log("Exit: "+ process.pid);
+        for(var i=0; i<5; i++) {
+            console.log("Destroying worker:  " + worker.process.pid);
+            workers[i].destroy();
+        }
+    });
+*/
+
 } else {
     http.createServer(function(req, res) {
         res.writeHead(200);
